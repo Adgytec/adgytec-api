78aa5e7bd4033d578d78b5193e6fe93746f76366
rohan <rohanverma031@gmail.com>
2024-08-30 11:21:22 +0530

    feat: added methods rleated to album photos

--- v1/services/gallery.go
+++ v1/services/gallery.go
@@ -25,6 +25,16 @@ type Album struct {
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 }
 
+type Photos struct {
+	Id        string    `json:"id" db:"photo_id"`
+	Path      string    `json:"image" db:"path"`
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+}
+
+type PhotosPath struct {
+	Path string `db:"path"`
+}
+
 func addAlbumToDatabase(a *Album, userId, projectId string, wg *sync.WaitGroup, errChan chan error) {
 	defer wg.Done()
 
@@ -303,4 +313,177 @@ func (a *Album) GetAlbumsByProjectId(projectId, cursor string) (*[]Album, error)
 	}
 
 	return &albums, nil
+}
+
+// photos
+
+func addPhotoToDatabase(p *Photos, userId, albumId string, wg *sync.WaitGroup, errChan chan error) {
+	defer wg.Done()
+
+	args := dbqueries.PostPhotoByAlbumIdArgs(p.Id, albumId, p.Path, userId)
+	_, err := db.Exec(ctx, dbqueries.PostPhotoByAlbumId, args)
+	if err != nil {
+		var pgErr *pgconn.PgError
+
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "23502" {
+				message := "Some required values are empty."
+				err = &custom.MalformedRequest{Status: http.StatusBadRequest, Message: message}
+				errChan <- err
+				return
+			}
+
+			if pgErr.Code == "23503" {
+				message := "Invalid user or album."
+				err = &custom.MalformedRequest{Status: http.StatusBadRequest, Message: message}
+				errChan <- err
+				return
+			}
+		}
+
+		log.Printf("Error adding photo in database: %v\n", err)
+	}
+
+	errChan <- err
+
+}
+
+func (p *Photos) PostPhotoByAlbumId(r *http.Request, projectId, albumId, userId string) (string, error) {
+	file, header, err := r.FormFile("cover")
+	if err != nil {
+		log.Printf("Error retriving file: %v\n ", err)
+		return "", err
+	}
+	defer file.Close()
+
+	contentType, err := isImageFile(header)
+	if err != nil {
+		return "", err
+	}
+
+	img, format, err := image.Decode(file)
+	if err != nil {
+		log.Printf("Error decoding image: %v\n", err)
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	err = handleImage(img, buf, format)
+	if err != nil {
+		return "", err
+	}
+
+	photoId := GenerateUUID().String()
+	objectName := fmt.Sprintf("services/gallery/%v/%v/photos/%v.%v", projectId, albumId, photoId, format)
+
+	if val := os.Getenv("ENV"); val == "dev" {
+		objectName = "dev/" + objectName
+	}
+	p.Path = objectName
+	p.Id = photoId
+
+	wg := new(sync.WaitGroup)
+	errChan := make(chan error, 2)
+
+	wg.Add(2)
+	go uploadImageToCloudStorage(objectName, buf, contentType, wg, errChan)
+	go addPhotoToDatabase(p, userId, albumId, wg, errChan)
+
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		if err != nil {
+			go deleteFromCloudStorage(objectName)
+			go p.DeletePhotoById([]string{p.Id})
+			return "", err
+		}
+	}
+
+	return photoId, nil
+}
+
+func (p *Photos) DeletePhotoById(photoId []string) error {
+	args := dbqueries.DeletePhotosByIdArgs(photoId)
+	rows, err := db.Query(ctx, dbqueries.DeletePhotosById, args)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "22P02" {
+				message := "Invalid photo ids."
+				return &custom.MalformedRequest{Status: http.StatusBadRequest, Message: message}
+			}
+		}
+		log.Printf("Error deleting photos from db: %v\n", err)
+		return err
+	}
+	defer rows.Close()
+
+	photos, err := pgx.CollectRows(rows, pgx.RowToStructByName[PhotosPath])
+	if err != nil {
+		log.Printf("Error reading rows: %v\n", err)
+		return err
+	}
+
+	if len(photos) == 0 {
+		return &custom.MalformedRequest{Status: http.StatusNotFound, Message: "Photos not found"}
+	}
+
+	objectChan := make(chan minio.ObjectInfo)
+	go func() {
+		defer close(objectChan)
+		for _, img := range photos {
+			objectChan <- minio.ObjectInfo{Key: img.Path}
+		}
+	}()
+	e := spaceStorage.RemoveObjects(ctx, os.Getenv("SPACE_STORAGE_BUCKET_NAME"), objectChan, minio.RemoveObjectsOptions{})
+
+	isErr := false
+	for err := range e {
+		log.Printf("Error deleting objects in space storage, %v\n", err)
+		isErr = true
+	}
+
+	if isErr {
+		return errors.New("error deleting image from space storage")
+	}
+
+	return nil
+}
+
+func (p *Photos) GetPhotosByAlbumId(albumId, cursor string) (*[]Photos, error) {
+	args := dbqueries.GetPhotosByAlbumIdArgs(albumId, cursor)
+	rows, err := db.Query(ctx, dbqueries.GetPhotosByAlbumId, args)
+
+	if err != nil {
+		log.Printf("Error fetching photos from db: %v\n", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	photos, err := pgx.CollectRows(rows, pgx.RowToStructByName[Photos])
+	if err != nil {
+		log.Printf("Error reading rows: %v\n", err)
+		return nil, err
+	}
+
+	wg := new(sync.WaitGroup)
+	urlChan := make(chan IndexedValue, len(photos))
+
+	for ind, item := range photos {
+		wg.Add(1)
+
+		img := item.Path
+		go generatePresignedUrl(img, ind, expires, wg, urlChan)
+	}
+
+	wg.Wait()
+	close(urlChan)
+
+	for url := range urlChan {
+		ind := url.Index
+		photos[ind].Path = url.Url
+	}
+
+	return &photos, nil
 }
